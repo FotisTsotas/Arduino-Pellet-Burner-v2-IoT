3682552fd55a37b4142bb06689318663063998be
unknown <fotistsotas@yahoo.com>
2022-08-07 15:16:33 +0300

    'transimt'

--- master/master.ino
+++ master/master.ino
@@ -1,27 +1,37 @@
+#include <arduino-timer.h>
 #include <Wire.h>
-float myDelay = 23.5;  // this is for milliseconds
-int myInt = round(myDelay);
-byte high = highByte(myInt);
-byte low = lowByte(myInt);
-int  val = word(high, low);
 
 void setup() {
- Serial.begin(9600); /* begin serial comm. */
- Wire.begin(); /* join i2c bus as master */
- Serial.println("I am I2C Master");
+  Serial.begin(9600); /* begin serial comm. */
+  Wire.begin();       /* join i2c bus as master */
+  Serial.println("I am I2C Master");
 }
 
 void loop() {
- Wire.beginTransmission(8); /* begin with device address 8 */
- Wire.write(high);  /* sends hello string */
- Wire.write(low);  /* sends hello string */
- Wire.endTransmission();    /* stop transmitting */
 
- Wire.requestFrom(8, 9); /* request & read data of size 9 from slave */
- while(Wire.available()){
-    char c = Wire.read();/* read data received from slave */
-  Serial.print(c);
- }
-Serial.print(val);
- delay(1000);
+  Wire.requestFrom(8, 9); /* request & read data of size 9 from slave */
+  while (Wire.available()) {
+    char c = Wire.read(); /* read data received from slave */
+    Serial.print(c);
+  }
+  Serial.println();
+  delay(10);
+}
+
+void transmitData(int fireValue, int waterValue, int exhaustValue, int status) {
+  Wire.beginTransmission(8); /* begin with device address 8 */
+  sendDataToSlave("w", waterValue);
+  sendDataToSlave("f", fireValue);
+  sendDataToSlave("e", exhaustValue);
+  sendDataToSlave("s", status);
+  Wire.endTransmission(); /* stop transmitting */
+}
+
+void sendDataToSlave(String type, int value) {
+  int valueOfInt = round(value);
+  byte high = highByte(valueOfInt);
+  byte low = lowByte(valueOfInt);
+  Wire.write(high);         /* sends hello string */
+  Wire.write(low);          /* sends hello string */
+  Wire.write(type.c_str()); /* sends hello string */
 }
