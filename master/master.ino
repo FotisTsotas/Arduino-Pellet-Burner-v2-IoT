9bca44bc51e0acdf1c34b73dc010863fb2fe5fe3
FotisTsotas <FOTISTSOTAS@YAHOO.COM>
2022-08-09 23:37:33 +0300

    'addTimers'

--- master/master.ino
+++ master/master.ino
@@ -1,24 +1,52 @@
 #include <arduino-timer.h>
 #include <Wire.h>
+#include <arduino-timer.h>
+
+unsigned long previousMillis[5] = { 0, 0, 0, 0, 0 };//number of onOfftimers
+bool state[5] = { 0, 0, 0, 0, 0 };//number of onOfftimers
+auto timer = timer_create_default();
+float fireValue = 0;
+float exhaustValue = 0;
+float waterValue = 0;
+int status = 0;
 
 void setup() {
-  Serial.begin(9600); /* begin serial comm. */
-  Wire.begin();       /* join i2c bus as master */
+  Serial.begin(9600);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  Wire.begin();
   Serial.println("I am I2C Master");
+  timer.every(1000, transmitData);
 }
 
 void loop() {
-
-  Wire.requestFrom(8, 9); /* request & read data of size 9 from slave */
-  while (Wire.available()) {
-    char c = Wire.read(); /* read data received from slave */
-    Serial.print(c);
-  }
-  Serial.println();
-  delay(10);
+  // timer.tick();
+  onOffTimer(12, 2, 1, 2);
+  onOffTimer(11, 3, 1, 2);
+  // Wire.requestFrom(8, 9); /* request & read data of size 9 from slave */
+  // while (Wire.available()) {
+  //   char c = Wire.read(); /* read data received from slave */
+  //   Serial.print(c);
+  // }
+  // Serial.println();
 }
 
-void transmitData(int fireValue, int waterValue, int exhaustValue, int status) {
+void onOffTimer(int relayPin, int i, int long onTime, long offTime) {
+  unsigned long currentMillis = millis();
+  onTime = onTime * 1000;
+  offTime = offTime * 1000;
+  if ((state[i] == HIGH) && (currentMillis - previousMillis[i] >= onTime)) {
+    state[i] = LOW;
+    previousMillis[i] = currentMillis;
+    digitalWrite(relayPin, state[i]);
+  } else if ((state[i] == LOW) && (currentMillis - previousMillis[i] >= offTime)) {
+    state[i] = HIGH;
+    previousMillis[i] = currentMillis;
+    digitalWrite(relayPin, state[i]);
+  }
+}
+
+void transmitData() {
   Wire.beginTransmission(8); /* begin with device address 8 */
   sendDataToSlave("w", waterValue);
   sendDataToSlave("f", fireValue);
