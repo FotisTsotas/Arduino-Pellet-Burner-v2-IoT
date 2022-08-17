713515acbab226c1db62cab030dfe3bd14c56b26
unknown <fotistsotas@yahoo.com>
2022-08-18 00:53:37 +0300

    node as master

--- master/master.ino
+++ master/master.ino
@@ -1,72 +1,141 @@
-#include <arduino-timer.h>
 #include <Wire.h>
-#include <arduino-timer.h>
+#include <WiFiClient.h>
+#include <ThingSpeak.h>
+#include <ESP8266WiFi.h>
 
-unsigned long previousMillis[5] = { 0, 0, 0, 0, 0 };  //number of onOfftimers
-bool state[5] = { 0, 0, 0, 0, 0 };                    //number of onOfftimers
-auto timer = timer_create_default();
-float fireValue = 0;
-float exhaustValue = 0;
-float waterValue = 0;
-int status = 0;
-byte x = 0;
+const char *ssid = "VODAFONE_4041";
+const char *password = "r9fsfcscdfasf8ah";
+int valWater = 0;
+int valFire = 0;
+int valExeust = 0;
+int fireStatus = 0;
+int statusCode = 0;
+int correctTransmition = 0;
+const char *myWriteAPIKey = "QT1YBK5WMHL1EJKJ";
+const char *readAPi = "V8MMJ9CTRPGPHY90";
+unsigned long myChannelNumber = 1735462;
+unsigned long previousMillis = 0;
+int connection = 13;
+bool state = LOW;
+WiFiClient client;
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000;
+int relayInput = 2;
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  Wire.begin();
-  Serial.println("I am I2C Master");
-  timer.every(1000, transmitData);
+  pinMode(connection, OUTPUT);
+  pinMode(relayInput, OUTPUT);
+  Wire.begin(D1, D2); /* join i2c bus as master */
+  wifiConnection();
 }
 
 void loop() {
-  // timer.tick();
-  requestDataFromSlave();
-  delay(500);
-}
-
-void onOffTimer(int relayPin, int i, int long onTime, long offTime) {
-  unsigned long currentMillis = millis();
-  onTime = onTime * 1000;
-  offTime = offTime * 1000;
-  if ((state[i] == HIGH) && (currentMillis - previousMillis[i] >= onTime)) {
-    state[i] = LOW;
-    previousMillis[i] = currentMillis;
-    digitalWrite(relayPin, state[i]);
-  } else if ((state[i] == LOW) && (currentMillis - previousMillis[i] >= offTime)) {
-    state[i] = HIGH;
-    previousMillis[i] = currentMillis;
-    digitalWrite(relayPin, state[i]);
+  if ((millis() - lastTime) > timerDelay) {
+    collectData();
+    if (!checkTransmition()) {
+      collectData();
+    } else {
+      wiFiSendData();
+    }
+    lastTime = millis();
   }
 }
 
-void requestDataFromSlave() {
-  Wire.requestFrom(8, 1); /* request & read data of size 9 from slave */
-  int bytes = Wire.available();
-  Serial.print("Slave sent ");
-  Serial.print(bytes);
-  Serial.print(" of information\n");
-  for (int i = 0; i < bytes; i++) {
-    x = Wire.read();
-    Serial.print("Slave Sent: ");
-    Serial.print(x);
-    Serial.println();
+
+void wifiConnection() {
+  Serial.begin(115200); /* begin serial comm. */
+  Serial.println();
+  Serial.print("Wifi connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  Serial.println();
+  Serial.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(connection, HIGH);
   }
-}
-void transmitData() {
-  Wire.beginTransmission(8); /* begin with device address 8 */
-  sendDataToSlave("w", waterValue);
-  sendDataToSlave("f", fireValue);
-  sendDataToSlave("e", exhaustValue);
-  sendDataToSlave("s", status);
-  Wire.endTransmission(); /* stop transmitting */
+
+  Serial.println("Wifi Connected Success!");
+  Serial.print("NodeMCU IP Address : ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(connection, LOW);
+  ThingSpeak.begin(client);
 }
 
-void sendDataToSlave(String type, int value) {
-  int valueOfInt = round(value);
-  byte high = highByte(valueOfInt);
-  byte low = lowByte(valueOfInt);
-  Wire.write(high);         /* sends hello string */
-  Wire.write(low);          /* sends hello string */
-  Wire.write(type.c_str()); /* sends hello string */
+void wiFiSendData() {
+  ThingSpeak.setField(1, valWater);
+  ThingSpeak.setField(2, valFire);
+  ThingSpeak.setField(3, valExeust);
+  ThingSpeak.setField(4, fireStatus);
+  statusCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (statusCode == 200) {
+    Serial.println("Success");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " + String(statusCode));
+  }
+}
+
+bool wiFiTakeData(void *) {
+  Serial.println("wifiTake");
+
+  //  long statous = ThingSpeak.readLongField(myChannelNumber, 4, readAPi);
+  //  statusCode = ThingSpeak.getLastReadStatus();
+  //  if (statusCode == 200)
+  //  {
+  //    Serial.print("Temperature: ");
+  //    Serial.println(statous);
+  //  }
+  //  else
+  //  {
+  //    Serial.println("Unable to read channel / No internet connection");
+  //  }
+  //  return true;
+}
+
+void collectData() {
+  valWater = 0;
+  valFire = 0;
+  valExeust = 0;
+  fireStatus = 0;
+  correctTransmition = 0;
+  byte buf[12];
+  int n = Wire.requestFrom(8, 12);
+  for (int i = 0; i < n; i++) {
+    buf[i] = Wire.read();
+  }
+
+  byte highW = buf[0];
+  byte lowW = buf[1];
+  char typeW = buf[2];
+  valWater = word(highW, lowW);
+
+  byte highF = buf[3];
+  byte lowF = buf[4];
+  char typeF = buf[5];
+  valFire = word(highF, lowF);
+
+  byte highE = buf[6];
+  byte lowE = buf[7];
+  char typeE = buf[8];
+  valExeust = word(highE, lowE);
+
+  char typeS = buf[10];
+  byte valS = buf[9];
+  fireStatus = valS;
+  correctTransmition = buf[11];
+  Serial.println(correctTransmition);
+}
+
+bool checkTransmition() {
+  if (correctTransmition != 1) {
+    digitalWrite(relayInput, HIGH);
+    delay(1500);
+    digitalWrite(relayInput, LOW);
+    return false;
+  } else {
+    correctTransmition = 0;
+    return true;
+  }
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,138 +1,178 @@
-#include <Wire.h>
-// #include <WiFiClient.h>
-#include <ThingSpeak.h>
-#include <arduino-timer.h>
-// #include <ESP8266WiFi.h>
+#include <LiquidCrystal.h>
 
-const char *ssid = "Nova_2.4G_2QYhEcs";
-const char *password = "uG52kCNf";
-int waterValue = 0;
-int fireValue = 0;
-int exhaustValue = 0;
-int statous;
-int water;
-int exhust;
-int flame;
-int statusCode = 0;
-const char *myWriteAPIKey = "QT1YBK5WMHL1EJKJ";
-const char *readAPi = "V8MMJ9CTRPGPHY90";
-unsigned long myChannelNumber = 1735462;
-unsigned long previousMillis = 0;
-int ledPin = 13;
-bool state = LOW;
-// WiFiClient client;
-auto timer = timer_create_default(); 
+#include <Wire.h>
+#include <arduino-timer.h>
+#include <EEPROM.h>
+#include <max6675.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_SI2C.h>
+#define ONE_WIRE_BUS 5
+const int flameSensor = A0;
+const int voltageSensor = A2;
+const int motorAir = 1;
+const int beginResistor = 2;
+const int motorPellet = 6;
+const int onOffTimmers = 7;
+const int buzz = 13;
+int ktcSO = 8;
+int ktcCS = 9;
+int ktcCLK = 10;
+int volt = 0;
+int status = 0;
+float vOUT = 0.0;
+float vIN = 0.0;
+float R1 = 30000.0;
+float R2 = 7500.0;
+float fireValue = 0;
+float exhaustValue = 0;
+float waterValue = 0;
+float firePerCent;
+unsigned long previousMillis[5] = { 0, 0, 0, 0, 0 };  //number of onOfftimers
+unsigned long pelletInDelayLastTime = 0;
+unsigned long pelletInDelay = 10000;
+bool state[5] = { 0, 0, 0, 0, 0 };
+auto timer = timer_create_default();
+bool open = false;
+bool ignition = false;
+bool manual = true;
+bool menu = false;
+
+
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 void setup() {
-  Wire.begin(8);                /* join i2c bus with address 8 */
-  Wire.onReceive(receiveEvent); /* register receive event */
-  Wire.onRequest(requestEvent); /* register request event */
-  Serial.begin(9600);           /* start serial comm. */
-  Serial.println("I am I2C Slave");
-  timer.every(10000, wiFiTakeData);
-  timer.every(10000, wiFiSendData);
-  //  wifiConnection();
+  relay(1, 1, 1, 1);
+  pinMode(motorAir, OUTPUT);
+  pinMode(beginResistor, OUTPUT);
+  pinMode(motorPellet, OUTPUT);
+  pinMode(onOffTimmers, OUTPUT);
+  pinMode(buzz, OUTPUT);
+  lcd.begin(16, 2);
+  sensors.begin();
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
 }
 
 void loop() {
-
-  timer.tick();  // tick the timer
+  collectDataFromSensors();
+  if (open) {
+    mainMode();
+  } else {
+    closed();
+  }
+  // timer.tick();
 }
 
-void onOffTimer(int relayPin, int long onTime, long offTime) {
+void mainMode() {
+  if (!ignition) {
+    pelletIn();
+  } else {
+    mainWorking();
+  }
+}
+
+void pelletIn() {
+  if ((millis() - pelletInDelayLastTime) > pelletInDelay) {
+    relay(1, 1, 0, 1);
+    timeOn = round(millis() - pelletInDelayLastTime) / 1000)
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("EISAGWGI PELLET");
+    lcd.setCursor(0, 1);
+    lcd.print(timeOn);
+    lcd.print(" sec");
+  }
+  pelletInDelayLastTime = millis();
+}
+
+void mainWorking() {
+}
+
+void closed() {
+}
+
+void menuMode() {
+}
+
+void collectDataFromSensors() {
+  sensors.requestTemperatures();
+  waterValue = sensors.getTempCByIndex(0);
+  exhaustValue = analogRead(A1);  // tha mpei o  aisthtiras kausaeriwn
+  fireValue = analogRead(flameSensor);
+  volt = analogRead(voltageSensor);
+  vOUT = (volt * 5.0) / 1024.0;
+  vIN = vOUT / (R2 / (R1 + R2));
+  if (vIN > 12) {
+    open = true;
+  } else {
+    open = false;
+  }
+}
+
+void relay(bool air, bool resist, bool motorpellet, bool onofftimers) {
+  digitalWrite(motorAir, air);
+  digitalWrite(beginResistor, resist);
+  digitalWrite(motorPellet, motorpellet);
+  digitalWrite(onOffTimmers, onofftimers);
+}
+
+void onOffTimer(int relayPin, int i, int long onTime, long offTime) {
   unsigned long currentMillis = millis();
   onTime = onTime * 1000;
   offTime = offTime * 1000;
-  if ((state == HIGH) && (currentMillis - previousMillis >= onTime)) {
-    state = LOW;
-    previousMillis = currentMillis;
-    digitalWrite(relayPin, state);
-  } else if ((state == LOW) && (currentMillis - previousMillis >= offTime)) {
-    state = HIGH;
-    previousMillis = currentMillis;
-    digitalWrite(relayPin, state);
+  if ((state[i] == HIGH) && (currentMillis - previousMillis[i] >= onTime)) {
+    state[i] = LOW;
+    previousMillis[i] = currentMillis;
+    digitalWrite(relayPin, state[i]);
+  } else if ((state[i] == LOW) && (currentMillis - previousMillis[i] >= offTime)) {
+    state[i] = HIGH;
+    previousMillis[i] = currentMillis;
+    digitalWrite(relayPin, state[i]);
   }
 }
 
 
-void wifiConnection() {
-  //  Serial.begin(115200);
-  //  Serial.println();
-  //  Serial.print("Wifi connecting to ");
-  //  Serial.println(ssid);
-  //  WiFi.begin(ssid, password);
-  //  Serial.println();
-  //  Serial.print("Connecting");
-
-  //  while (WiFi.status() != WL_CONNECTED)
-  //  {
-  //    delay(500);
-  //    Serial.print(".");
-  //    digitalWrite(LED_BUILTIN, HIGH);
-  //  }
-
-  //  Serial.println("Wifi Connected Success!");
-  //  Serial.print("NodeMCU IP Address : ");
-  //  Serial.println(WiFi.localIP());
-  //  digitalWrite(LED_BUILTIN, LOW);
-  //  ThingSpeak.begin(client);
-}
-bool wiFiSendData(void *) {
-    Serial.println("wifiSend");
-
-  // ThingSpeak.setField(1, fireValue);
-  // ThingSpeak.setField(2, waterValue);
-  // ThingSpeak.setField(3, exhaustValue);
-  // statusCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-  // if (statusCode == 200) {
-  //   Serial.println("Success");
-  // } else {
-  //   Serial.println("Problem updating channel. HTTP error code " + String(statusCode));
+void receiveEvent(int howMany) {
+  // while (0 < Wire.available()) {
+  //   char c = Wire.read();
+  //   Serial.print(c);
   // }
-  // return true;
-}
-
-bool wiFiTakeData(void *) {
-    Serial.println("wifiTake");
-
-  //  long statous = ThingSpeak.readLongField(myChannelNumber, 4, readAPi);
-  //  statusCode = ThingSpeak.getLastReadStatus();
-  //  if (statusCode == 200)
-  //  {
-  //    Serial.print("Temperature: ");
-  //    Serial.println(statous);
-  //  }
-  //  else
-  //  {
-  //    Serial.println("Unable to read channel / No internet connection");
-  //  }
-  //  return true;
-}
-
-void receiveEvent() {
-  int val = 0;
-  while (1 < Wire.available()) {
-    byte high = Wire.read();
-    byte low = Wire.read();
-    val = word(high, low);
-    char type = Wire.read();
-    String typeString = String(type);
-    if (typeString == "w") {
-      waterValue = val;
-    } else if (typeString == "f") {
-      fireValue = val;
-    } else if (typeString == "e") {
-      exhaustValue = val;
-    } else {
-      hasStatus(typeString);
-    }
-  }
-}
-
-void hasStatus(String status) {
-  Serial.println(status);
+  // Serial.println();
 }
 
 void requestEvent() {
-  Wire.write("Hi Master"); /*send string on request */
+  sendDataToMaster(waterValue, fireValue, exhaustValue, status);
+}
+
+void sendDataToMaster(int valueW, int valueF, int valueE, int valueS) {
+  int valueOfIntW = round(valueW);
+  int valueOfIntF = round(valueF);
+  int valueOfIntE = round(valueE);
+  byte highW = highByte(valueOfIntW);
+  byte lowW = lowByte(valueOfIntW);
+  byte highF = highByte(valueOfIntF);
+  byte lowF = lowByte(valueOfIntF);
+  byte highE = highByte(valueOfIntE);
+  byte lowE = lowByte(valueOfIntE);
+  byte buffer[12];
+  buffer[0] = highW;
+  buffer[1] = lowW;
+  buffer[2] = "w";
+  buffer[3] = highF;
+  buffer[4] = lowF;
+  buffer[5] = "f";
+  buffer[6] = highE;
+  buffer[7] = lowE;
+  buffer[8] = "e";
+  buffer[9] = valueS;
+  buffer[10] = "s";
+  buffer[11] = 1;
+  Wire.write(buffer, 12);
 }
